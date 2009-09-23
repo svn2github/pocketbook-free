89a408762846bf53a66e8ca509e2ee8ae1780744
evgenybf <evgenybf@9c927b23-cf09-4d9e-9ea5-a7a7176551bf>
2009-09-23 19:25:11 +0000

    Small experiment with libffi
    git-svn-id: https://svn.code.sf.net/p/pocketbook-free/svn@117 9c927b23-cf09-4d9e-9ea5-a7a7176551bf

---
+++ pyinkview/.tmp/test.py
@@ -0,0 +1,20 @@
+import a
+
+a.initialize_module()
+
+def proc1(a):
+    print ("Hello from proc1! ", a)
+
+def proc2(a):
+    print ("Hello from proc2! ", a)
+
+callback1 = a.Callback(proc1)
+callback2 = a.Callback(proc2)
+
+a.setCallback(callback1)
+a.callCallback()
+a.setCallback(callback2)
+a.callCallback()
+
+del callback1
+del callback2
